e66ecc6d3d0453849177f1f8665fa9f8653856a4
VitorCorrea18 <vitorcorrea18@gmail.com>
2022-09-06 19:31:15 -0300

    feat: implments the create method at patient service

--- src/services/PatientService.ts
+++ src/services/PatientService.ts
@@ -1,4 +1,4 @@
-import { IPatient, IPatientService, IPatientModel, IInstallmentModel } from '../protocols';
+import { IPatient, IPatientService, IPatientModel, IInstallmentModel, ITreatmentModel } from '../protocols';
 // import PatientModel from '../database/models/patientModel';
 import TreatmentModel from '../database/models/treatmentModel';
 import InstallmentModel from '../database/models/installmentModel';
@@ -30,9 +30,46 @@ export default class PatientServices implements IPatientService {
 
   async create(patient: IPatient, installments: number): Promise<IPatient> {
     const newPatient = await this.patientModel.create(patient);
-    const d = new Date();
-    const dataFormatada = ((d.getDate())) + "/" + ((d.getMonth() + 1)) + "/" + d.getFullYear();
-    console.log(dataFormatada, installments);
-    return newPatient as IPatient;
+    const newInstallments = await this.createInstallments(installments, newPatient.id);
+
+    const result = { ...newPatient.dataValues, installments: newInstallments };
+    return result as IPatient;
   }
+
+  async createInstallments(installments: number, patientId: number | undefined) {
+    let d = new Date();
+    const startDate = (d.getFullYear() + "-" + ("0" + (d.getMonth() + 1)).slice(-2) + "-" + ("0" + (d.getDate())).slice(-2));
+    const installmentsArray: [string] = [startDate];
+
+    // https://stackoverflow.com/questions/65509371/add-1-month-to-current-date-in-javascript-can-you-please-integrate-your-solutio
+    for (let i = 1; i <= installments; i += 1) {
+      d = new Date(d.setMonth(d.getMonth() + 1));
+      const nextDate = (d.getFullYear() + "-" + ("0" + (d.getMonth() + 1)).slice(-2) + "-" + ("0" + (d.getDate())).slice(-2));
+      installmentsArray.push(nextDate);
+    }
+    const result = Promise.all(installmentsArray.map(async (d) => {
+      return await this.installmentModel.create({ date: d, status: 'Pendente', patientId });
+    }))
+    return result;
+  }
+
+  async getById(id: number): Promise<IPatient> {
+    const result = await this.patientModel.getById({
+      where: { id },
+      include: [
+        {
+          model: TreatmentModel,
+          as: 'treatment',
+          attributes: { exclude: ['id'] }
+        },
+        {
+          model: InstallmentModel,
+          as: 'installments',
+          attributes: { exclude: ['patientId'] }
+        },
+      ]
+    });
+    return result as IPatient;
+  }
+
 }
