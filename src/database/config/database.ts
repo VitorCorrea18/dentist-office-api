e633da3abe1b64625f6e878a3eada9bea8e007d1
VitorCorrea18 <vitorcorrea18@gmail.com>
2022-09-07 20:01:21 -0300

    chore: preparing to deploy

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -2,10 +2,10 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
+  username: process.env.DB_USER || 'b290e0b84085cf',
+  password: process.env.DB_PASS || '82a77c51',
   database: 'DRA_ERICA_DB',
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.CLEARDB_DATABASE_URL || '127.0.0.1',
   port: Number(process.env.DB_PORT) || 3002,
   dialect: 'mysql',
   dialectOptions: {
