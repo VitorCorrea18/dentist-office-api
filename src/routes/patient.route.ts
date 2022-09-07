67b053d63a89084f9c6bdbac31dec9597e5b5dbb
VitorCorrea18 <vitorcorrea18@gmail.com>
2022-09-06 21:59:47 -0300

    feat: implements Joi validation

--- src/routes/patient.route.ts
+++ src/routes/patient.route.ts
@@ -1,15 +1,17 @@
 import * as express from 'express';
 import { patientFactory } from '../factories';
+import { Validation } from '../middlewares/';
 
 
 const controller = patientFactory();
+const validation = new Validation();
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
   controller.getAll(req, res, next);
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', validation.patient, (req, res, next) => {
   controller.create(req, res, next);
 });
 
---
+++ src/schemas/index.ts
@@ -0,0 +1,2 @@
+export { default as patientSchema } from './patientSchema';
+export default {}; //  lint
