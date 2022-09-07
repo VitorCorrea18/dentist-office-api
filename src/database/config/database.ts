a041d794fab48a4d87db69ce06e306780c163463
VitorCorrea18 <vitorcorrea18@gmail.com>
2022-09-07 20:49:34 -0300

    chore: change .env DB_URL

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -5,7 +5,7 @@ const config: Options = {
   username: process.env.DB_USER || 'b290e0b84085cf',
   password: process.env.DB_PASS || '82a77c51',
   database: 'DRA_ERICA_DB',
-  host: process.env.CLEARDB_DATABASE_URL || '127.0.0.1',
+  host: process.env.DB_URL || '127.0.0.1',
   port: Number(process.env.DB_PORT) || 3002,
   dialect: 'mysql',
   dialectOptions: {
